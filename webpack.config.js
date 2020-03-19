f56080a6a323137dd6c9e80752e571dbc6835ad4
Jesper Pedersen <pejes@aarhus.dk>
2020-03-19 08:37:23 +0100

    IOTDEV-67: Add copying of static assets to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -68,4 +68,20 @@ Encore
 // .enableReactPreset()
 // .addEntry('admin', './assets/js/admin.js')
 
+
+
+// Enable copy of static assets
+  .copyFiles({
+    from: './assets/images',
+    // optional target path, relative to the output dir
+    to: 'images/[path][name].[ext]',
+
+    // if versioning is enabled, add the file hash too
+    to: 'images/[path][name].[hash:8].[ext]',
+
+    // only copy files matching this pattern
+    pattern: /\.(png|jpg|jpeg)$/
+  })
+
+
 module.exports = Encore.getWebpackConfig()
