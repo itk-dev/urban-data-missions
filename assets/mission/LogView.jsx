d0bd757f1952733c1c5b2aa1fc172dff55ab0acd
Mikkel Ricky <rimi@aarhus.dk>
2020-04-03 15:12:44 +0200

    IOTDEV-98: Added log entry filter

--- assets/mission/LogView.jsx
+++ assets/mission/LogView.jsx
@@ -2,12 +2,24 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Button from 'react-bootstrap/Button'
+import Badge from 'react-bootstrap/Badge'
+import Alert from 'react-bootstrap/Alert'
 
 class LogView extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      entries: []
+      entries: [],
+      filter: {
+        // Display order
+        names: ['user', 'alert', 'system'],
+        // Actual values
+        values: {
+          user: true,
+          system: true,
+          alert: true
+        }
+      }
     }
   }
 
@@ -50,7 +62,40 @@ class LogView extends Component {
     this.props.onHandleAddLogEntry && this.props.onHandleAddLogEntry(logEntry)
   }
 
+  applyFilter = (entry) => {
+    return this.state.filter.values[entry.type]
+  }
+
+  toggleFilter = (name) => {
+    const filter = this.state.filter
+
+    // At least one name must always be selected
+    const values = { ...filter.values }
+    values[name] = !values[name]
+    if (Object.values(values).filter(value => value).length < 1) {
+      return
+    }
+
+    filter.values = values
+    this.setState({ filter: filter })
+  }
+
   render () {
+    const renderFilter = (name) => {
+      const active = this.state.filter.values[name]
+
+      return (
+        <Badge
+          key={`filter-${name}`} pill
+          className={`filter-${name} ${active ? 'active' : ''} badge-log-entry-type badge-log-entry-type-${name}`}
+          onClick={() => this.toggleFilter(name)}
+        >{name}
+        </Badge>
+      )
+    }
+
+    const entries = this.state.entries.filter(this.applyFilter)
+
     return (
       <section className='log-view'>
         <header className='row my-3'>
@@ -63,22 +108,32 @@ class LogView extends Component {
                 <span className='fas fa-plus' />
               </Button>}
           </div>
+
+          <div className='col'>
+            {/* @TODO: Design */}
+            Filter types
+            <div className='btn-group btn-group-toggle'>
+              {this.state.filter.names.map(renderFilter)}
+            </div>
+          </div>
         </header>
 
         <div className='log-view-content'>
-          {this.state.entries.map((entry, index) => (
-            <article className={`row mb-3 log-entry log-entry-${entry.type}`} key={'log-entry-' + index}>
-              <div className='col-auto'>
-                <p className='small mb-0 text-primary-faded pt-1'>{entry.loggedAt}</p>
-                <span className={`badge badge-pill badge-sm badge-log-entry-type badge-log-entry-type-${entry.type}`}>{entry.type}</span>
-              </div>
-              <div className='col'>
-                <p>{entry.content}</p>
-                {/* @TODO: Design */}
-                {entry.sensor && <div className='sensor'>{entry.sensor.id}</div>}
-              </div>
-            </article>
-          ))}
+          {entries.length === 0
+            ? <Alert variant='warning'>No log entries</Alert>
+            : entries.map((entry, index) => (
+              <article className={`row mb-3 log-entry log-entry-${entry.type}`} key={'log-entry-' + index}>
+                <div className='col-auto'>
+                  <p className='small mb-0 text-primary-faded pt-1'>{entry.loggedAt}</p>
+                  <span className={`badge badge-pill badge-sm badge-log-entry-type badge-log-entry-type-${entry.type}`}>{entry.type}</span>
+                </div>
+                <div className='col'>
+                  <p>{entry.content}</p>
+                  {/* @TODO: Design */}
+                  {entry.sensor && <div className='sensor'>{entry.sensor.id}</div>}
+                </div>
+              </article>
+            ))}
         </div>
       </section>
     )
