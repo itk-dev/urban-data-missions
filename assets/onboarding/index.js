e58a3b969f2205d09e101f5f1ff4ff0ad661f6df
Mikkel Ricky <rimi@aarhus.dk>
2020-05-01 17:35:18 +0200

    IOT-105: Added missing icons

--- assets/onboarding/index.js
+++ assets/onboarding/index.js
@@ -1,8 +1,14 @@
 /* Import this components styling */
 import './onboarding.scss'
 
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons'
+
 /* Require roboto typeface */
 require('typeface-roboto')
 require('jquery')
 require('popper.js')
 require('bootstrap')
+
+library.add(faArrowLeft, faArrowRight)
+dom.watch()
