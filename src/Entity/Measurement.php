f568a73b49d59eb9762d635c0bf840245d8815c0
Mikkel Ricky <rimi@aarhus.dk>
2021-01-04 15:54:58 +0100

    Fixed deprecation warning

--- src/Entity/Measurement.php
+++ src/Entity/Measurement.php
@@ -18,7 +18,7 @@ use Symfony\Component\Serializer\Annotation\Groups;
  *       "pagination_client_items_per_page"=true
  *     },
  *     collectionOperations={
- *       "get"={"maximum_items_per_page"=1000}
+ *       "get"={"pagination_maximum_items_per_page"=1000}
  *     },
  *     itemOperations={"get"},
  *     normalizationContext={"groups"={"measurement_read"}},
